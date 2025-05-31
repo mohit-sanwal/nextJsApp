540dfa2c8de3848249c08a1ebb8b81f09140b930
mohitSanwal <mohit123ongraph@gmail.com>
2025-06-01 00:17:27 +0530

    routing implementation

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+import styles from "./page.module.css";
+
+export default function Authenticate() {
+  return (
+    <div >
+      <main>
+         <h1> About page  </h1>
+      </main>
+      <footer>
+           footer section
+      </footer>
+    </div>
+  );
+}
