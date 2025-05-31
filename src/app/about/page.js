c5e3db38a3a10702a93f44d29c5cace31dd50d95
mohitSanwal <mohit123ongraph@gmail.com>
2025-06-01 01:00:48 +0530

    routing via useRouter

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,19 @@
+"use client"
 import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 
 export default function Authenticate() {
+    const router = useRouter();
+    const goToHome = (name) => {
+      router.push(name);
+    }
   return (
     <div >
       <main>
          <h1> About page  </h1>
       </main>
       <footer>
-           footer section
+           footer section <button onClick={() => goToHome('/')}>go to home</button>
       </footer>
     </div>
   );
