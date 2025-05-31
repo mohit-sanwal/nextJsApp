a6780e8b73068b86f8bbf3c5881522017cfc2296
mohitSanwal <mohit123ongraph@gmail.com>
2025-06-01 01:04:08 +0530

    Next js routing (#4)
    * routing implementation
    
    * routing via useRouter
    
    * using nested routing

---
+++ src/app/authenticate/page.js
@@ -0,0 +1,14 @@
+import styles from "./page.module.css";
+
+export default function About() {
+  return (
+    <div>
+      <main>
+         <h1> SingIn/SignUp page </h1>
+      </main>
+      <footer>
+           footer section
+      </footer>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,18 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
+ 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
          <h1> Home page </h1>
+         <Link href='/about'> <span className={styles.link}> about </span> </Link>
       </main>
       <footer className={styles.footer}>
-           footer section
+           footer section 
       </footer>
     </div>
   );
