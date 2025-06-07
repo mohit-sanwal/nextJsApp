557e699bc7cd673cfb6cd1c7a0508b71a1190f6b
mohitSanwal <mohit123ongraph@gmail.com>
2025-06-07 11:00:09 +0530

    404 page not found page added

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+import Link from "next/link";
+
+export default function PageNotFound() {
+ 
+  return (
+    <div className={styles.page}>
+        This page is not found
+    </div>
+  );
+}
