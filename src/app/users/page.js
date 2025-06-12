6f65d4b8ca0882edf03a5102e08e1f3b9070c62f
mohitSanwal <mohit123ongraph@gmail.com>
2025-06-12 13:26:09 +0530

    use client component with server component

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,19 +1,21 @@
+import ProfileButton from "./profileButton";
 
 async function getUsers() {
     let users = await fetch('https://jsonplaceholder.typicode.com/users');
     users = await users.json();
-    console.log('usrers', users);
     return users;
 }
 export default async function Users() {
     const users = await getUsers();
-  console.log('usrers-----> ', users);
   return (
     <div>
       {
         users.length && users.map((user) => {
             return (
-                <h6 key={user.id}>{user.name}</h6>
+              <div key={user.id}>
+                <h6>{user.name}</h6>
+                <ProfileButton user={user}/>
+              </div>
             )
         })
       }
